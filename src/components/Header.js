4c072b167155985b1ce551356e7d229bb3fb7f6c
ChiragKr04 <62651867+ChiragKr04@users.noreply.github.com>
2021-01-16 19:21:09 +0530

    Second

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
 
             <div className="header__center">
                 <p>Model S</p>
-                <p>Model 3</p>
+                <p>Model 4</p>
                 <p>Model X</p>
                 <p>Model Y</p>
                 <p>Solar Roof</p>
